a1d02088d78eeb128e314b129daa3866efe53876
jcoliver <jcoliver@email.arizona.edu>
2017-01-11 13:29:24 -0700

    Abstract function for iterative ordinal logistic regression

--- code/analyses-topic-preferences.R
+++ code/analyses-topic-preferences.R
@@ -31,6 +31,30 @@ topics.wide$topic <- factor(x = topics.wide$topic)
 topics.wide$preference <- ordered(x = topics.wide$preference, levels = c(1, 2, 3, 4, 5))
 
 ################################################################################
+# A function to run ordinal logistic regression for each level in a factor
+iterate.olr <- function(response, predictor, data) {
+  library("MASS")
+  orig.levels <- levels(data[, predictor])
+  values.matrix <- matrix(NA, length(orig.levels), length(orig.levels))
+  rownames(values.matrix) <- colnames(values.matrix) <- orig.levels
+  olr.results <- list("p.values" = values.matrix, "t.values" = values.matrix)
+  
+  for (i in 1:length(orig.levels)) {
+    data[, predictor] <- relevel(x = data[, predictor], ref = orig.levels[i])
+    olr <- polr(formula = data[, response] ~ data[, predictor], Hess = TRUE)
+    olr.coeff <- coef(summary(olr))
+    olr.p <- pnorm(abs(olr.coeff[, "t value"]), lower.tail = FALSE) * 2
+    olr.coeff <- cbind(olr.coeff, "p.value" = olr.p)
+    olr.results[["p.values"]][levels(data[, predictor])[-1], i] <- olr.coeff[1:length(x = orig.levels) - 1, "p.value"]
+    olr.results[["t.values"]][levels(data[, predictor])[-1], i] <- olr.coeff[1:length(x = orig.levels) - 1, "t value"]
+  }
+
+  adj.p <- 0.05/((length(orig.levels) * (length(orig.levels) - 1)) / 2)
+  olr.results$adj.p <- adj.p
+  return(olr.results)  
+}
+
+################################################################################
 # Test for differences among TOPICS
 
 # Kruskal Wallis omnibus test for an affect of topic on preference
@@ -41,32 +65,17 @@ kw.topic <- kruskal.test(x = topics.wide$preference, g = topics.wide$topic)
 # data:  topics.wide$preference and topics.wide$topic
 # Kruskal-Wallis chi-squared = 15.952, df = 11, p-value = 0.1429
 
-################################################################################
+########################################
 # Ordinal logistic regression, full set of comparisons, using each topic as 
 # reference
-library("MASS")
-topic.levels <- levels(topics.wide$topic)
-topic.values.matrix <- matrix(NA, length(x = topic.levels), length(x = topic.levels))
-rownames(topic.values.matrix) <- colnames(topic.values.matrix) <- topic.levels
-topic.olr.results <- list("p.values" = topic.values.matrix, "t.values" = topic.values.matrix)
 
-for (i in 1:length(x = topic.levels)) {
-  topics.wide$topic <- relevel(x = topics.wide$topic, ref = topic.levels[i])
-  olr <- polr(formula = preference ~ topic, data = topics.wide, Hess = TRUE)
-  olr.coeff <- coef(summary(olr))
-  olr.p <- pnorm(abs(olr.coeff[, "t value"]), lower.tail = FALSE) * 2
-  olr.coeff <- cbind(olr.coeff, "p.value" = olr.p)
-  topic.olr.results[["p.values"]][levels(topics.wide$topic)[-1], i] <- olr.coeff[1:(length(x = topic.levels) - 1), "p.value"]
-  topic.olr.results[["t.values"]][levels(topics.wide$topic)[-1], i] <- olr.coeff[1:(length(x = topic.levels) - 1), "t value"]
-}
-# Bonferroni correction would be a/m (a = 0.05, m is number of comparisons, or 
-# n(n-1)/2)
-topic.adj.p <- 0.05/((length(x = topic.levels) * (length(x = topic.levels) - 1)) / 2)
-# 0.00076
+topic.olr <- iterate.olr(response = "preference", predictor = "topic", data = topics.wide)
 
-# Print p-values matrix only if there are any significant values
-if (any(topic.olr.results[["p.values"]] < topic.adj.p, na.rm = TRUE)) {
-  print(topic.olr.results)
+# Print results only if at least one was significant
+if (any(topic.olr$p.values < topic.olr$adj.p, na.rm = TRUE)) {
+  print(topic.olr)
+} else {
+  print("No significant results in ordered logistic regression for TOPIC")
 }
 
 ################################################################################
@@ -79,28 +88,15 @@ kw.pos <- kruskal.test(x = topics.wide$preference, g = topics.wide$position)
 # data:  topics.wide$preference and topics.wide$position
 # Kruskal-Wallis chi-squared = 58.621, df = 2, p-value = 1.865e-13
 
-################################################################################
-# Ordinal logistic regression, full set of comparisons, using each topic as 
+########################################
+# Ordinal logistic regression, full set of comparisons, using each position as 
 # reference
-pos.levels <- levels(topics.wide$position)
-pos.values.matrix <- matrix(NA, length(x = pos.levels), length(x = pos.levels))
-rownames(pos.values.matrix) <- colnames(pos.values.matrix) <- pos.levels
-pos.olr.results <- list("p.values" = pos.values.matrix, "t.values" = pos.values.matrix)
 
-for (i in 1:length(x = pos.levels)) {
-  topics.wide$position <- relevel(x = topics.wide$position, ref = pos.levels[i])
-  olr <- polr(formula = preference ~ position, data = topics.wide, Hess = TRUE)
-  olr.coeff <- coef(summary(olr))
-  olr.p <- pnorm(abs(olr.coeff[, "t value"]), lower.tail = FALSE) * 2
-  olr.coeff <- cbind(olr.coeff, "p.value" = olr.p)
-  pos.olr.results[["p.values"]][levels(topics.wide$position)[-1], i] <- olr.coeff[1:(length(x = pos.levels) - 1), "p.value"]
-  pos.olr.results[["t.values"]][levels(topics.wide$position)[-1], i] <- olr.coeff[1:(length(x = pos.levels) - 1), "t value"]
-}
+pos.olr <- iterate.olr(response = "preference", predictor = "position", data = topics.wide)
 
-pos.adj.p <- 0.05/((length(x = pos.levels) * (length(x = pos.levels) - 1)) / 2)
-# 0.0167
-
-# Print p and t matrices only if there is at least one significant result
-if (any(pos.olr.results[["p.values"]] < pos.adj.p, na.rm = TRUE)) {
-  print(pos.olr.results)
+# Print results only if at least one was significant
+if (any(pos.olr$p.values < pos.olr$adj.p, na.rm = TRUE)) {
+  print(pos.olr)
+} else {
+  print("No significant results in ordered logistic regression")
 }
