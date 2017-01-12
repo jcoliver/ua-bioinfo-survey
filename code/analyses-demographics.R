2efd58192e8c4b02694b48ad71971d2257530daf
jcoliver <jcoliver@email.arizona.edu>
2017-01-12 11:20:20 -0700

    Distribute dual appointments into each College

--- code/analyses-demographics.R
+++ code/analyses-demographics.R
@@ -28,6 +28,13 @@ counts <- as.numeric(college.table)
 ids[1] <- "Other / not listed"
 new.data <- data.frame(category = "College", id = ids, count = counts)
 
+# Want to distribute dual appointments
+sci.and.med <- which(new.data$id == "Science & Medicine")
+sci.med.count <- new.data$count[sci.and.med]
+new.data$count[which(new.data$id == "Science")] <- new.data$count[which(new.data$id == "Science")] + sci.med.count
+new.data$count[which(new.data$id == "Medicine")] <- new.data$count[which(new.data$id == "Medicine")] + sci.med.count
+new.data <- new.data[-sci.and.med, ]
+
 table.data <- rbind(table.data, new.data)
 
 ################################################################################
@@ -46,7 +53,7 @@ table.data <- rbind(table.data, time.data)
 table.data$percent <- 100 * round(table.data$count / completed, 3)
 
 write.table(x = table.data, file = "output/table-demographics.txt", quote = FALSE, sep = "\t", row.names = FALSE)
-#    category                                        id count percent
+# category                                        id count percent
 # 1  Position                                   Faculty    33    45.8
 # 2  Position                                     Staff    15    20.8
 # 3  Position                                   Student    21    29.2
@@ -54,14 +61,14 @@ write.table(x = table.data, file = "output/table-demographics.txt", quote = FALS
 # 5   College                        Other / not listed     7     9.7
 # 6   College Arizona Biological and Biomedical Program     1     1.4
 # 7   College                            BIO5 Institute     1     1.4
-# 8   College                                  Medicine    43    59.7
+# 8   College                                  Medicine    44    61.1
 # 9   College                                   Nursing     2     2.8
 # 10  College                                  Pharmacy     7     9.7
 # 11  College                             Public Health     6     8.3
-# 12  College                                   Science     4     5.6
-# 13  College                        Science & Medicine     1     1.4
-# 14     Time                                       0-5    31    43.1
-# 15     Time                                      5-10    13    18.1
-# 16     Time                                     10-20    13    18.1
-# 17     Time                                       >20     6     8.3
-# 18     Time                                 Not given     9    12.5
+# 12  College                                   Science     5     6.9
+# 13     Time                                       0-5    31    43.1
+# 14     Time                                      5-10    13    18.1
+# 15     Time                                     10-20    13    18.1
+# 16     Time                                       >20     6     8.3
+# 17     Time                                 Not given     9    12.5
+# > 
